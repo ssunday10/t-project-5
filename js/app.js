e18290bfd4740d067dda8951d37e055317aec8e2
Shaylene Sunday <allkatt@yahoo.com>
2024-05-03 13:06:46 -0500

    fixed the baguettebox

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-baguetteBox.run('.gallery');​
+document.addEventListener('', function() {
+    baguetteBox.run('.gallery');
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    baguetteBox.run('.gallery');
+        var inputField = document.getElementById('inputFieldId');
+        var thumbnailsContainer = document.getElementById('thumbnails');
+
+        inputField.addEventListener('keyup', function(event) {
+            if (event.target.value.length >= 3) { // Example condition
+                var thumbnails = thumbnailsContainer.getElementsByTagName('img');
+                for (var i = 0; i < thumbnails.length; i++) {
+                    thumbnails[i].addEventListener('click', function() {
+                        var mainImage = document.getElementsByTagName('a');
+                        mainImage.src = this.src.replace('thumbnail', 'fullsize');
+                    });
+                }
+            }
+        });
+});
