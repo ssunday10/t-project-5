1e9c3ac33c11f9fda40d0f2bfce4360480785823
Shaylene Sunday <allkatt@yahoo.com>
2024-05-06 13:14:29 -0500

    updated the html and app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-const search = new Filter('search', 'data-caption');
 baguetteBox.run('.gallery');
 
-const search = document.getElementById('search');
- search.addEventListener('keyup', e=> {
-    let currentValue = e.target.value.toLowerCase();
-        let captions =document.querySelectorAll('a');
-        captions.forEach(caption => {
-        console.log(caption.getAttribute("data-caption"));
-     });
-});
+const search = document.getElementById('search', 'data-caption');
+search.addEventListener('keyup', e=> {
+   let currentValue = e.target.value.toLowerCase();
+   let captions = document.querySelectorAll('a');
+   captions.forEach (caption => {
+       while ("search", caption.textContent.includes(currentValue)) {
+         return search, caption.parentNode.style.display = "block";
+       } 
+       console.log(caption.getAttribute("data-caption"));
+      });
+   });
+
+
+   
