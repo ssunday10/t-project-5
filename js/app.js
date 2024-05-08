7d84c244205a5494ab89d9610ac180ff91fdf96e
Shaylene Sunday <allkatt@yahoo.com>
2024-05-08 13:54:53 -0500

    updated javascript

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
+const gallery = document.querySelector('.gallery');
 baguetteBox.run('.gallery');
 
-const search = document.getElementById('search').addEventListener('keyup', e => {
+const search = document.getElementById('search').addEventListener('keyup', (e) => {
+      // user input value
    let currentValue = e.target.value.toLowerCase();
    let captions = document.querySelectorAll('a');
-   captions.forEach (captions=> { 
-         if (search) {
-            return `${gallery}.show()`;
-          } else {
-               return `${gallery}.hide()`;
-          }
-   });
-   console.log(captions);
+
+   captions.forEach(caption => { 
+      let dataCaption = caption.getAttribute('data-caption');
+      if (search) {
+       dataCaption.includes(currentValue).parentNode.show();
+      }
 });
