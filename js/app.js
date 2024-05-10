30f31152ec267cacca7aa2c103414e2e9a0ae991
Shaylene Sunday <allkatt@yahoo.com>
2024-05-10 13:45:48 -0500

    fixed search bar. try to fix search filter

--- js/app.js
+++ js/app.js
@@ -4,10 +4,15 @@ baguetteBox.run('.gallery');
 const search = document.getElementById('search').addEventListener('keyup', (e) => {
       // user input value
    let currentValue = e.target.value.toLowerCase();
+   // searches captions
    let captions = document.querySelectorAll('a');
 
-   captions.forEach( (caption) => { 
+   captions.forEach((caption) => { 
       let dataCaption = caption.getAttribute('data-caption');
-      dataCaption.push('a');
+      if (currentValue === dataCaption) {
+          return "href".parentNode.style.display = "block";
+      } else {
+         null;
+      }
    });
 });
